2f27dc318b40746ce39a9ddb71464fc3331fc32a
Gonior <43252085+Gonior@users.noreply.github.com>
2019-10-24 03:27:48 +0700

    Dibuat : Kamis, 24 Oktober 2019

---
+++ src/pbo11k/pkg10118901/latihan54/koordinat/Koordinat.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118901.latihan54.koordinat;
+/**
+ * @author
+ * NAMA             : Dedi cahya
+ * NIM              : 10118901
+ * KELAS            : IF11K
+ * Deskripsi Program  : Program untuk menampilkan koordinat dan warnas konsep pewarisan class
+ */
+public class Koordinat {
+   protected int x,y;
+   
+    public Koordinat(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+   
+}
---
+++ src/pbo11k/pkg10118901/latihan54/koordinat/PBO11K10118901Latihan54Koordinat.java
@@ -0,0 +1,21 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118901.latihan54.koordinat;
+/**
+ * @author
+ * NAMA             : Dedi cahya
+ * NIM              : 10118901
+ * KELAS            : IF11K
+ * Deskripsi Program  : Program untuk menampilkan koordinat dan warnas konsep pewarisan class
+ */
+
+public class PBO11K10118901Latihan54Koordinat {
+
+    public static void main(String[] args) {
+        WarnaKoordinat w = new WarnaKoordinat(10, 4, "Jingga");
+    }
+    
+}
